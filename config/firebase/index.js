3a8c294739e6551c5d155f00ffc399107cee0042
Jaine Senna <jainesantossenna191@gmail.com>
2021-08-25 22:10:12 -0300

    Configurando firebase

---
+++ config/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAolcX-l2umFfA-2sC5LjKnFK4IMoR8NEo",
+  authDomain: "clockerwork01.firebaseapp.com",
+  projectId: "clockerwork01",
+  storageBucket: "clockerwork01.appspot.com",
+  messagingSenderId: "315092489072",
+  appId: "1:315092489072:web:48fe04cc08358c45aab5b5",
+  measurementId: "G-8ELH3TJF10"
+};
+
+export default firebase.getApps.length 
+  ? firebase.app() 
+  : firebase.initializeApp(firebaseConfig)
