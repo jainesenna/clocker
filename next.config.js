2b7387c43aeac538e1971c4456c954e4988ecec1
Jaine Senna <jainesantossenna191@gmail.com>
2021-08-27 16:28:51 -0300

    Update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 // module.exports = {
 //   reactStrictMode: true,
 // }
-
 module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
     });
 
     return config;
-  }
+  },
 };
