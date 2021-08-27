561abfe9c4810bd5c5604e380cca2953481a6dee
Jaine Senna <jainesantossenna191@gmail.com>
2021-08-27 19:15:16 -0300

    export firebase config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 // module.exports = {
 //   reactStrictMode: true,
 // }
+
 module.exports = {
+  reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
+      test: /.svg$/,
+      issuer:  { and: [/.(js|ts)x?$/] },
+
+      use: ["@svgr/webpack"],
     });
 
     return config;
   },
-};
+}
