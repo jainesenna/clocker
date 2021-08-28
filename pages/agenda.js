b87ccdb568a0df9e8ac74b7487957e124920e701
Jaine Senna <jainesantossenna191@gmail.com>
2021-08-28 20:04:58 -0300

    auth redirect with context

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Button } from '@chakra-ui/react'
+import { useAuth } from './../components'
+
 export default function Agenda() {
+  const [auth, {logout}] = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    !auth.user && router.push('/')
+  }, [auth.user])
+
   return (
-   <div>
-     Agenda
-   </div>
+    <div>
+        <Button colorScheme="red" onClick={logout}>Sair</Button>
+    </div>
   )
 }
